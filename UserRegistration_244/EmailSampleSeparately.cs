6bb21abfe3c30e5682fc6463386ed033f4d40fec
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-24 17:37:15 +0530

    [Shruti] Perform all email samples provided separately

---
+++ UserRegistration_244/EmailSampleSeparately.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration_244
+{
+    internal class EmailSampleSeparately
+    {
+        public void CheckName(string name)
+        {
+            string validateFirstName = "^[A-Z][a-z]{3,20}$";
+            try
+            {
+                if (Regex.IsMatch(name, validateFirstName))
+                    Console.WriteLine(" Name validate succesfull");
+                else
+                    Console.WriteLine("Invalid name!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        internal void CheckEmail(string email)
+        {
+            string validateEmail = "^[a-zA-Z0-9]+[._+-]{0,1}[0-9a-zA-Z]+[@][a-zA-Z]+[.][a-zA-Z]{2,3}([.][a-zA-Z]{2,3}){0,1}$";
+            try
+            {
+                if (Regex.IsMatch(email, validateEmail))
+                    Console.WriteLine(" email validate succesfull");
+                else
+                    Console.WriteLine("Invalid email!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        public void CheckMobileNo(string mobile)
+        {
+            string validateMobile = "^[0-9]{1,4}\\s[0-9]{10,12}$";
+            try
+            {
+                if (Regex.IsMatch(mobile, validateMobile))
+                    Console.WriteLine(" mobile no validate succesfull");
+                else
+                    Console.WriteLine("Invalid mobile no.!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+        internal void CheckPassword(string password)
+        {
+            string validatepassword = "^(?=.*[A-Z])(?=.*[0-9])(?=.*[&%$#@^*!~]).{8,}$";
+            try
+            {
+                if (Regex.IsMatch(password, validatepassword))
+                    Console.WriteLine("password validate succesfull");
+                else
+                    Console.WriteLine("Invalid password!!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- UserRegistration_244/Program.cs
+++ UserRegistration_244/Program.cs
@@ -77,17 +77,38 @@
 
 
             //UR8
-            Console.Write("Enter The Password:");
-            String pass = Console.ReadLine();
+            //Console.Write("Enter The Password:");
+            //String pass = Console.ReadLine();
 
-            if (Pass1SpecialCharacter.validate(pass))
-            {
-                Console.WriteLine("Valid");
-            }
-            else
-            {
-                Console.WriteLine("Invalid");
-            }
+            //if (Pass1SpecialCharacter.validate(pass))
+            //{
+            //    Console.WriteLine("Valid");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid");
+            //}
+
+            //UR9
+            EmailSampleSeparately regex = new EmailSampleSeparately();
+            Console.WriteLine("User registration Problem statement");
+            Console.WriteLine("*First letter should be capital.");
+            Console.WriteLine("*length should be Minimum three characters.");
+            Console.WriteLine("Enter First name :");
+            string fname = Console.ReadLine();
+            regex.CheckName(fname);
+            Console.WriteLine("Enter Last name :");
+            string lname = Console.ReadLine();
+            regex.CheckName(lname);
+            Console.WriteLine("Enter Email id :");
+            string email = Console.ReadLine();
+            regex.CheckEmail(email);
+            Console.WriteLine("Enter Mobile no. :");
+            string mobile = Console.ReadLine();
+            regex.CheckMobileNo(mobile);
+            Console.WriteLine("Enter Password :");
+            string password = Console.ReadLine();
+            regex.CheckPassword(password);
 
 
 
