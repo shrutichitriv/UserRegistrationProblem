46a41d30e9add975dfe5b7734b98517f6a78a31e
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-24 16:21:28 +0530

    [Shruti] Perform User registration program

---
+++ UserRegistration_244/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace UserRegistration_244
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WelCome to User Registration Program");
+
+        }
+    }
+}
+
