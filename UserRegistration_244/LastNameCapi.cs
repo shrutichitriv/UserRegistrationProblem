e212b0302c13fd23f3ab549c6b555318640cb567
shrutichitriv <shrutichitriv199620@gmail.com>
2023-01-24 16:44:30 +0530

    [Shruti] Perform to enter a valid last name

---
+++ UserRegistration_244/LastNameCapi.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration_244
+{
+    internal class LastNameCapi
+    {
+        public static bool LastNameValidation(string LName)
+        {
+            string pattern = "^[A-Z][a-z]{2,}$"; // Last name validation
+            // name entered  is match with regex then it is valid otherwise not
+            if (Regex.IsMatch(LName, pattern))
+            {
+                Console.WriteLine($"\nYour Last name  valid=" + LName);
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"\nYour Last name  not valid=" + LName);
+                return false;
+            }
+        }
+    }
+}
--- UserRegistration_244/Program.cs
+++ UserRegistration_244/Program.cs
@@ -7,10 +7,16 @@
             Console.WriteLine("WelCome to User Registration Program");
 
             //UR1
-            Console.WriteLine("Welcome to Regular Expression");
-            Console.WriteLine("Enter your first name");
-            string FirstName = Console.ReadLine(); // Creating user input for first name
-            FirstNameCapi.FirstNameValidation(FirstName);//Calling method to validate firstname
+            //Console.WriteLine("Welcome to Regular Expression");
+            //Console.WriteLine("Enter your first name");
+            //string FirstName = Console.ReadLine(); // Creating user input for first name
+            //FirstNameCapi.FirstNameValidation(FirstName);//Calling method to validate firstname
+
+            //UR2
+            Console.WriteLine("Enter your Last name");
+            string LName = Console.ReadLine(); //  input for last name
+            LastNameCapi.LastNameValidation(LName);
+
 
 
         }
